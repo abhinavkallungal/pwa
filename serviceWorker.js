5ef4f5b7d888b4f71f3a94354f7cbad95adf5b28
ABHINAV K <abhinavkallungal15@gmail.com>
2021-08-15 14:05:05 +0530

    almost done

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,11 +6,11 @@ var urlsToCache = [
     "/img/icon512.png"
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
+self.addEventListener('install', e=>{
+  
+  e.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(cache =>{
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
@@ -18,57 +18,6 @@ self.addEventListener('install', function(event) {
 });
 
 
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-  
-          return fetch(event.request).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-  
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
-  
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-  
-              return response;
-            }
-          );
-        })
-      );
-  });
-
-  self.addEventListener('activate', function(event) {
-
-    var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-  
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheAllowlist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
-
-
-
-
+self.addEventListener('fetch',e=>{ 
+  console.log(`inretsepting fetch request for: ${e.request.url}`);
+})
